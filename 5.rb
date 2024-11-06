39c7938d5546b0e277a2ab7e395584abdd04cf1f
lazykapo <sideorder921@gmail.com>
2024-11-06 22:54:07 +0900

    if → unless

---
+++ 5.rb
@@ -0,0 +1,15 @@
+unless a + b <= 0
+  puts "計算結果は0より大きいです"
+end
+
+# if 条件式
+   #条件式がtrueの時に実行する処理
+# else
+   #条件式がfalseの時に実行する処理
+# end
+
+# unless 条件式
+   #条件式がfalseの時に実行する処理
+# else
+   #条件式がtrueの時に実行する処理
+# end
